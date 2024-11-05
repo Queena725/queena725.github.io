7f976ca8c1fbc0af3b559dcd211d859cd97163aa
Queena725 <leeq370@newschool.edu>
2024-11-05 15:20:19 -0500

    Update script(8).js

--- script(8).js
+++ script(8).js
@@ -1,1 +1,9 @@
-
+`Yellow roses - friendship and joy
+an excellent choice for expressing appreciation to a teacher who has been a guiding light.
+Hydrangeas - gratitude, grace, beauty 
+Sunflowers -  positivity, joy, adoration, bright, cheerful appearance
+Carnations -  love, fascination, distinction
+Yellow tulips - cheerful thoughts, making a bright and joyful gift for a teacher. 
+Peonies - prosperity, honor, romance, great choice for expressing deep admiration and respect.
+Alstroemerias -  friendship, devotion, and prosperity
+a perfect way to express your appreciation for a teacher who has shown you unwavering support and guidance.
