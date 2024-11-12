69e65629bbd8be983eab910b6a734c8f8e5d9360
Queena725 <leeq370@newschool.edu>
2024-11-12 16:25:57 -0500

    Update script(6).js

--- script(6).js
+++ script(6).js
@@ -1,1 +1,13 @@
+const text = "Uh-oh! Did things get a little heated? Time to bring out the flowers and say, ‘I’m sorry’ in style!";
+const typingElement = document.getElementById("typing-text");
+let index = 0;
 
+function typeText() {
+  if (index < text.length) {
+    typingElement.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeText, 100); 
+  }
+}
+
+window.onload = typeText;
