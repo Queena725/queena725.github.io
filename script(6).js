2ae341d646624fdffc52df2547aa4a29cded5038
Queena725 <leeq370@newschool.edu>
2024-11-12 16:26:49 -0500

    Update script(6).js

--- script(6).js
+++ script(6).js
@@ -1,13 +1,73 @@
-const text = "Uh-oh! Did things get a little heated? Time to bring out the flowers and say, ‘I’m sorry’ in style!";
-const typingElement = document.getElementById("typing-text");
-let index = 0;
-
-function typeText() {
-  if (index < text.length) {
-    typingElement.innerHTML += text.charAt(index);
-    index++;
-    setTimeout(typeText, 100); 
-  }
+html{
+	box-sizing: border-box;	
 }
 
-window.onload = typeText;
+
+.image-container {
+    position: relative;
+    display: inline-block;
+}
+
+.image-container img {
+    width: 100%; 
+    display: block;
+}
+
+#typing-text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%); 
+    font-size: 24px;
+    color: white; 
+    text-align: center;
+    border-right: 2px solid white; 
+    white-space: nowrap;
+    overflow: hidden;
+	 font-family: "Playwrite TZ", cursive;
+  font-optical-sizing: auto;
+  font-weight: ;
+  font-style: normal;
+}
+
+
+
+  
+body{
+	width: 100%;
+	margin: 0;
+	padding: 0;
+		font-family: Arial, Helvetica, sans-serif;
+	  }
+
+h1{
+	position: fixed;
+	width: 100%;
+	font-size:250%;
+	color: #fff;
+	text-align: center;
+}
+section{
+	background-repeat: no-repeat;
+	background-attachment:fixed;
+	background-position: center top;
+	width: 100%;
+	height: 100vh;
+}
+
+.I{
+background-image:url(images/whiterose.jpg);
+}
+.II{
+	background-image: url(images/blue-hydrage.jpg);
+}
+.III{
+	background-image: url(images/orchids.JPG);
+}
+.IV{
+	background-image: url(images/purple\ orchids.JPG);
+}
+.V{
+	background-image: url(images/yellowrose.jpg);
+}
+
