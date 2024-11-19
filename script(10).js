044eda532098274c5ad5cc22676c4cb72697e96b
Queena725 <leeq370@newschool.edu>
2024-11-19 14:51:36 -0500

    Update script(10).js

--- script(10).js
+++ script(10).js
@@ -1,65 +1,58 @@
-h1 {
-    position: fixed; 
-    top: 0; 
-    width: 100%; 
-    font-size: 250%;
-    color: #38381f;
-    text-align: center;
-    z-index: 10; 
-    background-color: ; 
-    padding: 10px 0; 
-    box-shadow: 0 4px 6px rgba(94, 94, 94, 0.1); 
+const text = `1st Anniversary 
+Carnations, depicting youthful and passionate love.
+
+5th Anniversary
+Daisies, representing innocence and gentleness.
+
+10th Anniversary
+Daffodils, symbolising renewal and the promise of future happiness.
+
+15th Anniversary
+Roses, embodying love, appreciation, and the strength of the relationship that has blossomed over fifteen years.
+
+25th Anniversary
+Irises, representing wisdom, and valour, compliment the silver milestone of marriage with their elegant blooms.
+
+50th Anniversary
+Violets, paired with yellow roses, signify the golden jubilee of love, virtue, humility, and faithfulness.
+
+!`;
+
+
+let index = 0;
+const typingSpeed = 25;
+
+
+function type() {
+    if (index < text.length) {
+        document.getElementById('typing-text').innerHTML += text.charAt(index);
+        index++;
+        setTimeout(type, typingSpeed);
+    }
 }
 
-p {
-    color: #575b34;
-    font-size: 1rem;
-    text-align: center;
-    margin-top: 100px; 
-}
+window.onload = () => {
+    type();
 
+   
+    const hoverTexts = document.querySelectorAll('.hover-text');
+    const popupImage = document.getElementById('popupImage');
+    const popupImg = document.getElementById('popupImg');
 
+    hoverTexts.forEach(text => {
+        text.addEventListener('mouseenter', (e) => {
+            const imgSrc = text.getAttribute('data-image');
+            popupImg.src = imgSrc;
+            popupImage.style.display = 'block';
 
+    
+            const rect = text.getBoundingClientRect();
+            popupImage.style.left = `${rect.left}px`;
+            popupImage.style.top = `${rect.bottom + window.scrollY}px`;
+        });
 
-
-
-body {
-    font-family: 'Pacifico', cursive; 
-    background-color: #fdf5e6; 
-    margin: 0;
-    padding: 20px;
-}
-
-
-.hover-text-container {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr)); /
-    gap:20px;
-    text-align: center;
-    margin-top: 30px;
-}
-
-
-.hover-text {
-    font-size: 1rem; 
-    color: #697763; 
-    cursor: pointer;
-    transition: transform 0.3s ease, color 0.3s ease; 
-}
-
-.hover-text:hover {
-    color: #ffec3f66;
-    transform: rotate(5deg) scale(1.1); 
-}
-
-
-.popup-image img {
-    max-width: 700px; 
-    max-height: 700px; 
-    object-fit: contain; 
-    border-radius: ; 
-}
-
-
-
-  
+        text.addEventListener('mouseleave', () => {
+            popupImage.style.display = 'none';
+        });
+    });
+};
