1e8097c364ba8b804d07af74f94adf3aa9429b5f
Queena725 <leeq370@newschool.edu>
2024-11-19 14:55:14 -0500

    Update script(3).js

--- script(3).js
+++ script(3).js
@@ -1,1 +1,8 @@
 
+function displayRandomFlowers() {
+    let Flowers = ["🌸", "🌺", "🌻", "🌼", "🍀", "💐", "🌹", "🌷", "🏵️"];
+    let randomFlowers = Flowers[Math.floor(Math.random() * Flowers.length)];
+    document.getElementById("Flowers").innerText = randomFlowers;
+  }
+
+
