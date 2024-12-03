60aeb761bdcee6dd0444a92b0336ddeff3ae06a4
Queena725 <leeq370@newschool.edu>
2024-12-03 15:43:01 -0500

    Update script(1).js

--- script(1).js
+++ script(1).js
@@ -1,1 +1,48 @@
 
+document.addEventListener("scroll", function () {
+    const flowerImage = document.querySelector(".falling-flower");
+    const triggerPoint = flowerImage.offsetTop - window.innerHeight / 1.5;
+  
+    if (window.scrollY > triggerPoint) {
+      flowerImage.classList.add("show");
+    }
+  });
+  
+  
+  function randomizeFlower(flower) {
+    const screenWidth = window.innerWidth;
+    const randomLeft = Math.random() * screenWidth;
+    const randomDuration = Math.random() * 5 + 5; 
+    const randomDelay = Math.random() * 5; 
+  
+    flower.style.left = `${randomLeft}px`;
+    flower.style.animationDuration = `${randomDuration}s`;
+    flower.style.animationDelay = `${randomDelay}s`;
+    flower.style.opacity = 0;  
+    flower.style.animationPlayState = "paused";  
+  }
+  
+  
+  const flowers = document.querySelectorAll('.falling-flower');
+  
+  flowers.forEach((flower) => {
+    randomizeFlower(flower);
+  });
+  
+  
+  const triggerImage = document.querySelector('.trigger');
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+           
+            flowers.forEach((flower) => {
+                flower.style.opacity = 2; 
+                flower.style.animationPlayState = "running"; 
+            });
+        }
+    });
+  });
+  
+  
+  observer.observe(triggerImage);
+  
