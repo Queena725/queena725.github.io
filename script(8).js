781908750560a493668b7bc49c232fbde73f6c76
Queena725 <leeq370@newschool.edu>
2024-12-03 15:49:00 -0500

    Update script(8).js

--- script(8).js
+++ script(8).js
@@ -12,14 +12,19 @@ now scroll down!`;
 
 let index = 0;
 const typingSpeed = 25; 
+const delayAfterTyping = 5000; 
 
 function type() {
     if (index < text.length) {
         document.getElementById('typing-text').innerHTML += text.charAt(index);
         index++;
         setTimeout(type, typingSpeed);
+    } else {
+       
+        setTimeout(() => {
+            document.getElementById('typing-text').innerHTML = '';
+        }, delayAfterTyping);
     }
 }
 
 window.onload = type;
-
